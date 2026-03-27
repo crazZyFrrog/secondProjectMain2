93aad7e75f05e7abf7032e969d1e4060917a231d
Vladislav <vladislavlevonenko@gmail.com>
2026-03-27 17:41:42 +0300

    V9.56

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createRoutesFromChildren,
+  matchRoutes,
+  useLocation,
+  useNavigationType,
+} from 'react-router-dom'
+import * as Sentry from '@sentry/react'
 import App from './App'
 import './index.css'
+
+const sentryDsn = import.meta.env.VITE_SENTRY_DSN
+if (sentryDsn) {
+  Sentry.init({
+    dsn: sentryDsn,
+    enabled: import.meta.env.PROD,
+    integrations: [
+      Sentry.browserTracingIntegration(),
+      Sentry.reactRouterV6BrowserTracingIntegration({
+        useEffect,
+        useLocation,
+        useNavigationType,
+        createRoutesFromChildren,
+        matchRoutes,
+      }),
+    ],
+    tracesSampleRate: import.meta.env.PROD ? 0.1 : 0,
+  })
+}
 
 // Одноразовая очистка устаревших ключей авторизации
 if (!localStorage.getItem('_auth_cleared')) {
